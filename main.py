3cfc09e96553f678fbbabcfcd85c0bc1fb304a4e
Nicroxio <Ferdi@beacroft-mitchell.co.uk>
2022-09-07 17:29:03 +0100

    Finaly got the damn logic down... I think

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def clear():
     os.system('clear' if os.name != 'nt' else 'cls')
 
 def screen(grid):
-    for x in grid:
-        print(x)
+    for row in grid:
+        print(row)
 
-def grid(xlen,ylen):
+def gen_grid(xlen,ylen):
     grid =[]
     for num in range(0,ylen):
         grid.append([])
@@ -25,15 +25,56 @@ def random_placer(grid,Random_num):
         y = random.randint(0,len(grid)-1)
         grid[x][y] = '■'
 
+def cell_checker(grid):
+    check_cells =[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+    count = 0
+    new_grid = gen_grid(len(grid),len(grid[0]))
+    for row in range(0,len(grid)):
+        for col in range(0,len(grid[0])):
+
+            for coord in check_cells:
+                one,two = coord
+                new_row = row + one
+                new_col = col + two
+                # print(grid[new_row][new_col])
+                # print(count)
+                try:
+
+                    if grid[new_row][new_col] == '■':
+                        count=count+1
+                        pass
+
+                    if grid[new_row][new_col] == '□':
+                        pass
+                except:
+                    pass
+
+            if count == 2 and grid[row][col] == '■':
+                new_grid[row][col] = '■'
+            elif count == 3:
+                new_grid[row][col] = '■'
+            else:
+                new_grid[row][col] = '□'
+            count = 0
+
+
+    return new_grid
 
 
 
 if __name__ == '__main__':
     #init
-    grid = grid(int(input('How many Rows: ')), int(input('How many Columns: ')))
-    random_placer(grid,10)
+    grid = gen_grid(int(20), int(20))
+    random_placer(grid,40)
+    # screen(grid)
+    # grid = cell_checker(grid)
+    # screen(grid)
 
     #main loop
     while True:
         screen(grid)
+        grid = cell_checker(grid)
+        sleep(1)
         clear()
+        # exit()
+        pass
